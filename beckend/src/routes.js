833d7d32c85c8673badde62a344ee85fce0ce5f1
Rafael Araujo <rafarauj1@publicisgroupe.net>
2020-02-03 14:20:30 +0000

    alteração erro de api

--- beckend/src/routes.js
+++ beckend/src/routes.js
@@ -1,13 +1,28 @@
 const { Router } = require('express');
 const axios = require ('axios');
 const routes = Router();
+const Dev = require('./models/Dev');
+routes.post('/devs', async (request, response, next) => {
+  try{
+    const { github_username, techs } = request.body;
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    const {name = login, avatar_url, bio } = apiResponse.data;
+    const techsArray = techs.split(',').map(tech => tech.trim());
+    const dev = await Dev.create({
+      github_username,
+      name,
+      avatar_url,
+      bio,
+      techs: techsArray,
+    })
 
-routes.post('/devs', async (request, response) => {
-  const { github_username } = request.body;
-  const apiResponse = await axios.get(`https://api/github.com/users/${github_username}`);
-  console.log(apiResponse.data);
-
-  return response.json({message: "Hello Rafael, estou vivo!"});
+    console.log(name, avatar_url, bio, github_username);
+  
+    return response.json(dev);
+  } catch (err) {
+    next(err);
+  }
+  
 });
 
 module.exports = routes;
