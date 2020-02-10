8b992cd0706f172f88ac63431bef03bfcfbf2226
Rafael Araujo <rafarauj1@publicisgroupe.net>
2020-02-10 18:16:20 +0000

    incrementando a parte web

--- beckend/src/index.js
+++ beckend/src/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://rhakan:energetic17@cluster0-lt2vo.mongodb.net/se
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
